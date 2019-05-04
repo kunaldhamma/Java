7d5f2297e6962ab146d1bbcc84789a4f699f4ca8
kunaldhamma <kunal.dhamma@gmail.com>
2019-05-04 12:53:26 +0530

    Bubble And Selection Sort

---
+++ src/com/kunzen/java/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.kunzen.java;
+
+public class BubbleSort {
+	static int [] a = {4,6,1,9,63,2,78,34};
+  
+	static int[] bubbleSort(int [] a){
+		
+		int i,j,temp;
+		int l = a.length;
+		for(i=0;i<l;i++){
+			boolean swap=false;
+			for(j=0;j<l-i-1;j++){
+				if(a[j]>a[j+1]){
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+				swap=true;
+			}
+			if(swap==false)
+				break;
+		}
+		
+		return a;
+	}
+	
+	public static void main(String[] args) {
+	  int [] sortRes = bubbleSort(a);
+	  System.out.println("Sorted Arra=>");
+	  for(int i:sortRes){
+		  System.out.print(i+",");
+	  }
+
+	}
+}
---
+++ src/com/kunzen/java/SelectionSort.java
@@ -0,0 +1,43 @@
+package com.kunzen.java;
+
+/**
+ * 
+ * @author kunze
+ * Selection Sort Unstable version
+ * Unstable means order of occurence of the same values in the array is not maintained after sorting.
+ */
+public class SelectionSort {
+	static int [] a = {4,6,1,9,63,2,78,34};
+  
+	static int[] selectionSort(int [] a){
+		
+		int i,j,min_idx,temp;
+		int n=a.length;
+		
+		for(i=0;i<n-1;i++){
+			min_idx=i;
+			
+			for(j=i+1;j<n;j++){
+				if(a[j]<a[min_idx]){
+					min_idx=j;
+				}
+			}
+			
+			temp=a[i];
+			a[i]=a[min_idx];
+			a[min_idx]=temp;
+			
+		}
+		
+		return a;
+	}
+	
+	public static void main(String[] args) {
+	  int [] sortRes = selectionSort(a);
+	  System.out.println("Sorted Array=>");
+	  for(int i:sortRes){
+		  System.out.print(i+",");
+	  }
+
+	}
+}
